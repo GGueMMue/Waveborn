7fdfc200080ea26392fbab97acb1a06014c6e0a1
GGueMMue <daehan121993@gmail.com>
2025-06-09 03:47:22 +0900

    애니메이터 연결

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] Vector2 keyboardInputVector2d;
     [SerializeField] Rigidbody2D playerRB;
-
+    [SerializeField] SPUM_Prefabs playerAnim;
     public float characterSpeed;
 
     private void Awake()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<SPUM_Prefabs>();
     }
     // Start is called before the first frame update
     void Start()
@@ -28,6 +29,12 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if(keyboardInputVector2d != Vector2.zero)
+        {
+            playerAnim.PlayAnimation(1);
+        }
+        else playerAnim.PlayAnimation(0);
+
         Vector2 moveVector = keyboardInputVector2d.normalized * characterSpeed * Time.fixedDeltaTime;
         playerRB.MovePosition(playerRB.position + moveVector);
     }
