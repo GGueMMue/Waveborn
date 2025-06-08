7eea6a0364bde7ec42dfb64167336bebd34c5e5d
GGueMMue <daehan121993@gmail.com>
2025-06-09 06:34:35 +0900

    적 유닛 회전 보간 코드 추가

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -7,14 +7,15 @@ public class EnemyScript : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D playerRB;
 
+    [SerializeField] Quaternion lerpRot;
+    [SerializeField] float lerpSpeed = 10f;
+
     [SerializeField] bool _isDead;
 
     [SerializeField] Rigidbody2D rb;
-    SpriteRenderer spriteRenderer;
 
     private void Awake()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
     }
     // Start is called before the first frame update
@@ -29,6 +30,22 @@ public class EnemyScript : MonoBehaviour
         Vector2 moveVector = dir.normalized * moveSpeed * Time.fixedDeltaTime;
         rb.MovePosition(moveVector + rb.position);
         rb.velocity = Vector2.zero;
+    }
+
+    private void LateUpdate()
+    {
+        if (playerRB.position.x < rb.position.x)
+        {
+            lerpRot = Quaternion.Euler(0f, 180f, 0f);
+        }
+        else
+        {
+            lerpRot = Quaternion.Euler(0f, 0f, 0f);
+        }
+        rb.transform.rotation = Quaternion.Lerp(
+            rb.transform.rotation,
+            lerpRot,
+            Time.fixedDeltaTime* lerpSpeed);
     }
     // Update is called once per frame
     void Update()
