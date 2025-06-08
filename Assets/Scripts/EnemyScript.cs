bbb65c17e0a4618194e63b1d91500cfec267ff5f
GGueMMue <daehan121993@gmail.com>
2025-06-09 06:31:27 +0900

    오류 수정 + 적 스크립트

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -16,20 +16,19 @@ public class EnemyScript : MonoBehaviour
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
-
-        playerRB = GameManager.instance.pc.GetComponent<Rigidbody2D>();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GameManager.instance.pc.GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
         Vector2 dir = playerRB.position - rb.position;
         Vector2 moveVector = dir.normalized * moveSpeed * Time.fixedDeltaTime;
-
+        rb.MovePosition(moveVector + rb.position);
+        rb.velocity = Vector2.zero;
     }
     // Update is called once per frame
     void Update()
